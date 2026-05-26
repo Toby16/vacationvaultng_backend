a36fd2cd96b36d3974ec65e4de50d49de80a7a4c
Toby 👹 <bolajibaasit14@gmail.com>
2026-05-26 16:49:07 +0100

    Update pydantic_models.py

--- VACATIONVAULTNG/pydantic_models.py
+++ VACATIONVAULTNG/pydantic_models.py
@@ -3,17 +3,17 @@ from pydantic import BaseModel, EmailStr, Field
 
 
 class NEW_PROPERTY_LISTING_PYDANTIC(BaseModel):
-    title: str = Field(examples=["2bdrm Block of Flats in Horizon Cyberville, Ikate-Elegushi for rent"])
+    title: str = Field(examples=["3 bedroom Block of Flats in Horizon Cyberville, Ikate-Elegushi for rent"])
     description: str = Field(examples=["A nice 2 bedroom in a serene environment of Lekki just off the coastal road."])
     location: str = Field(examples=["Horizon Cyberville estate off Coastal Road, Ikate Lekki"])
     property_type: str = Field(examples=["Block of Flats"])
-    bedrooms: int = Field(examples=["2"])
-    bathrooms: int = Field(examples=["2"])
-    max_guests: int = Field(examples=["3"])
+    bedrooms: int = Field(examples=["3"])
+    bathrooms: int = Field(examples=["4"])
+    max_guests: int = Field(examples=["4"])
     weeks_per_year: int = Field(examples=["52"])
-    price: str = Field(examples=["450000"])
-    original_price: str = Field(examples=["510000"])
-    year_built: int = Field(examples=["2025"])
+    price: str = Field(examples=["45000"])
+    original_price: str = Field(examples=["51000"])
+    year_built: int = Field(examples=["2026"])
     status: str = Field(examples=["non-active"])
 
 class UPDATE_PROPERTY_LISTING_PYDANTIC(BaseModel):
